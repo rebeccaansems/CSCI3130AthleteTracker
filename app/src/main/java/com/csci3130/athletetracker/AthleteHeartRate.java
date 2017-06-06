efe644a8fb5c66581c6296960497b9ee1fb2fa68
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-06-06 12:14:41 -0300

    Added basics of heart rate tracking and range
    Rebecca & Chaoran

--- app/src/main/java/com/csci3130/athletetracker/AthleteHeartRate.java
+++ app/src/main/java/com/csci3130/athletetracker/AthleteHeartRate.java
@@ -5,4 +5,32 @@ package com.csci3130.athletetracker;
  */
 
 public class AthleteHeartRate {
+
+    private int currentHeartRate;
+    public int heartRateMin, heartRateMax;
+
+    //This allows for testing without Hexiwear data
+    public void setCurrentHeartRate(int heartRate){
+        currentHeartRate = heartRate;
+    }
+
+    public int getHeartRate(){
+        return currentHeartRate;
+    }
+
+    public void setHeartRateRange(int min, int max){
+        heartRateMin = min;
+        heartRateMax = max;
+    }
+
+    public boolean isHeartRateInRange(){
+        return isHeartRateInRange(currentHeartRate);
+    }
+
+    public boolean isHeartRateInRange(int heartRate){
+        if(heartRate > heartRateMin && heartRate < heartRateMax){
+            return true;
+        }
+        return false;
+    }
 }
